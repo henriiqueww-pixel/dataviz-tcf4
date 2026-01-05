ec9297a3e97d47d2b2cb1d1500702096cd1a9aba
henriiqueww-pixel <henriiqueww@gmail.com>
2026-01-05 08:30:08 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
-import streamlit as st
+# Importar biblioteca completa - padrão
+import io
+import unicodedata
 
+# Importar biblioteca completa - terceiro
+import joblib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import requests
+import shap
+
+import streamlit as st
+st.set_page_config(
 st.title('🍟 analise de Risco de Obesidade')
 
 st.info('Este aplicativo visa evidenciar as situações de risco analisadas de acordo com o banco de dados!')
-
+)
