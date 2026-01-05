21dc6f9383ab1b8a4eb90fdea4424289f5632e8a
henriiqueww-pixel <henriiqueww@gmail.com>
2026-01-05 08:27:58 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title('🍟 analise de Risco de Obesidade')
 
-st.write('Hello world!')
+st.info('Este aplicativo visa evidenciar as situações de risco analisadas de acordo com o banco de dados!')
 
