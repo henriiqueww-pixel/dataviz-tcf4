a8d06854dcb062b50163ad9a33ee0a49b5faa6e3
henriiqueww-pixel <henriiqueww@gmail.com>
2026-01-05 08:22:25 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,4 @@
 import streamlit as st
-# Importar biblioteca completa - padrão
-import io
-import unicodedata
-
-# Importar biblioteca completa - terceiro
-import joblib
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import requests
-import shap
 
 st.title('🎈🍟 aalise de Risco de Obesidade')
 
